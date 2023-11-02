a2bb4b5bfff59a64e49cf7bef5bfebc39a7d1ad0
Wei-Peng <30322673+pengwei-iie@users.noreply.github.com>
2023-11-02 20:59:19 +0800

    Update bash_train_ustc.sh

--- bash_train_ustc.sh
+++ bash_train_ustc.sh
@@ -1,1 +1,1 @@
-nohup python run_train.py --batch_size 48 --max_checkpoint 256 --breakpoint -1 --name_model bal_att_ustc_vocab --learning_rate 1e-3 --data_dir ./data/ustc > bal_att_ustc_vocab.out 2>&1 &
+
